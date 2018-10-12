607c868632039e03478961f3008b4e3f75afe3c1
Bhargav <bhargav960143@gmail.com>
2018-10-12 14:34:31 +0530

    Reset Password authentication and redirect to dashboard

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'securepanel';
+    protected $redirectTo = '/securepanel';
 
     /**
      * Create a new controller instance.
